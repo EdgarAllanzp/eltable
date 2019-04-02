98f247ae0be323bfa19a5fe0e8d523b9149e859d
EdgarAllanzp <edgarallan@qq.com>
2019-04-02 20:57:55 +0800

    generate fake data.

--- dev/mock.js
+++ dev/mock.js
@@ -1,3 +1,5 @@
+import dream from 'dreamjs';
+
 const option = {
   emptyText: '自定义暂无数据提示语',
   column: [
@@ -6,25 +8,51 @@ const option = {
       prop: 'name'
     },
     {
-      label: '职位',
-      prop: 'position'
+      label: '性别',
+      prop: 'gender'
     },
     {
-      label: '工作地点',
-      prop: 'office'
+      label: '公司',
+      prop: 'company'
+    },
+    {
+      label: '城市',
+      prop: 'city'
     },
     {
       label: '年龄',
       prop: 'age'
     },
     {
-      label: '入职时间',
-      prop: 'startDate'
+      label: '生日',
+      prop: 'birthday'
+    },
+    {
+      label: '邮箱',
+      prop: 'email'
     }
   ]
 };
 
-const data = [];
+dream.customType('pi', function () {
+  return Math.PI;
+});
+
+dream.customType('hello', /hello+ (world|to you)/);
+
+const data = dream
+  .schema({
+    id: 'guid',
+    name: 'name',
+    gender: 'gender',
+    company: 'company',
+    city: 'city',
+    age: 'age',
+    birthday: 'birthday',
+    email: 'email'
+  })
+  .generateRnd(100)
+  .output();
 
 export {
   option,
